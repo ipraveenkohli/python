a3f51a26c57bbe1c3a7fdae063010bb4df47ad7e
ipraveenkohli <praveenkohlilearner@gmail.com>
2026-02-19 22:47:42 +0530

    Create nested loop.py

---
+++ nested loop.py
@@ -0,0 +1,9 @@
+flatten a nested list using loops
+
+nested_list = [1, [2, 3], [4, 5, 6], 7, [8, 9]]
+
+# write function 'flatten_list' to flatten a nested list
+flattened = flatten_list(nested_list)
+print("Flattened list:", flattened)
+
+# Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
